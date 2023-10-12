560fd30c78b497c29f90b70bd7b5530229aebd40
iamshiv007 <iamshiv20032003@gmail.com>
2023-10-12 17:43:44 +0530

    database connected

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express")
+const connect = require("./database/connect")
 const app = express()
+require("dotenv").config()
 
 const port = process.env.PORT || 5000
 
@@ -7,6 +9,7 @@ app.listen(port, () => {
     console.log(`Server run on port http://localhost:${port}`)
 })
 
+connect()
 
 app.get("/", (req, res) => {
     res.send("Hello this is test message")
